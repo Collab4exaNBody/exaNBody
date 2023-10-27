12f93d89c2fc1e32297757f6bcdcb2272b4f03df
Thierry Carrard <thierry.carrard@cea.fr>
2023-10-27 11:04:36 +0200

    fixed compile error when particles have non trivial types (i.e. Vec3d, Mat3d, etc.) because of ONIKA_ATOMIC_ADD

--- src/exanb/grid_cell_particles/include/exanb/grid_cell_particles/cell_particle_update_functor.h
+++ src/exanb/grid_cell_particles/include/exanb/grid_cell_particles/cell_particle_update_functor.h
@@ -10,32 +10,56 @@ namespace exanb
 
   struct UpdateValueAdd
   {
-    template<class CellsT, bool ThreadSafe, typename... field_ids>
+    template< class T , bool ThreadSafe >
     ONIKA_HOST_DEVICE_FUNC
-    inline void operator()( CellsT cells, size_t cell_i, size_t p_i, const onika::soatl::FieldTuple<field_ids...>& in , onika::BoolConst<ThreadSafe> ) const
+    static inline void update_add_value( T& x , const T& a , onika::BoolConst<ThreadSafe> ts )
     {
       if constexpr ( ThreadSafe )
       {
-        ( ... , ( ONIKA_CU_ATOMIC_ADD( cells[cell_i][ onika::soatl::FieldId<field_ids>() ][p_i] , in[ onika::soatl::FieldId<field_ids>() ] , ONIKA_CU_MEM_ORDER_RELAXED ) ) );
+        ONIKA_CU_ATOMIC_ADD( x , a , ONIKA_CU_MEM_ORDER_RELAXED );
       }
-      else
+      if constexpr ( ! ThreadSafe )
       {
-        ( ... , ( cells[cell_i][ onika::soatl::FieldId<field_ids>() ][p_i] += in[ onika::soatl::FieldId<field_ids>() ] ) );
+        x += a;
       }
+    }
+
+    template< bool ThreadSafe >
+    ONIKA_HOST_DEVICE_FUNC
+    static inline void update_add_value( Vec3d& upd , const Vec3d& in , onika::BoolConst<ThreadSafe> ts )
+    {
+      update_add_value( upd.x , in.x , ts );
+      update_add_value( upd.y , in.y , ts );
+      update_add_value( upd.z , in.z , ts );
+    }
+
+    template< bool ThreadSafe >
+    ONIKA_HOST_DEVICE_FUNC
+    static inline void update_add_value( Mat3d& upd , const Mat3d& in , onika::BoolConst<ThreadSafe> ts )
+    {
+      update_add_value( upd.m11 , in.m11 , ts );
+      update_add_value( upd.m12 , in.m12 , ts );
+      update_add_value( upd.m13 , in.m13 , ts );
+      update_add_value( upd.m21 , in.m21 , ts );
+      update_add_value( upd.m22 , in.m22 , ts );
+      update_add_value( upd.m23 , in.m23 , ts );
+      update_add_value( upd.m31 , in.m31 , ts );
+      update_add_value( upd.m32 , in.m32 , ts );
+      update_add_value( upd.m33 , in.m33 , ts );
+    }
+
+    template<class CellsT, bool ThreadSafe, typename... field_ids>
+    ONIKA_HOST_DEVICE_FUNC
+    inline void operator()( CellsT cells, size_t cell_i, size_t p_i, const onika::soatl::FieldTuple<field_ids...>& in , onika::BoolConst<ThreadSafe> ts ) const
+    {
+      ( ... , ( update_add_value( cells[cell_i][ onika::soatl::FieldId<field_ids>() ][p_i] , in[ onika::soatl::FieldId<field_ids>() ] , ts ) ) );
     }
 
     template<bool ThreadSafe=false>
     ONIKA_HOST_DEVICE_FUNC
-    inline void operator() (double& upd, const double& in, onika::BoolConst<ThreadSafe> = {} ) const
+    inline void operator() (double& upd, const double& in, onika::BoolConst<ThreadSafe> ts = {} ) const
     {
-      if constexpr ( ThreadSafe )
-      {
-        ONIKA_CU_ATOMIC_ADD( upd , in , ONIKA_CU_MEM_ORDER_RELAXED );
-      }
-      else
-      {
-        upd += in;
-      }
+      update_add_value( upd , in , ts );
     }
   };
 
