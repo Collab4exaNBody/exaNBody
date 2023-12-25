85fd63cf624d54df941943f4d10c2501b6afd895
Thierry Carrard <thierry.carrard@cea.fr>
2023-12-25 18:44:27 +0100

    correctly indented pp directives encapsulated in pseudo macro calls

--- scripts/code-format.sh
+++ scripts/code-format.sh
@@ -6,16 +6,19 @@ sed -e 's/[ \t]*#[ \t]*pragma[ \t]\+\(.*\)/_Pragma(\"\1\");/g'   \
     -e 's/[ \t]*#[ \t]*if[ \t]*\(.*\)/PPIF_BEGIN(\"if \1\")/g'         \
     -e 's/[ \t]*#[ \t]*else/PPIF_END(\"else\")\nPPIF_BEGIN(\"else\")/g' \
     -e 's/[ \t]*#[ \t]*endif/PPIF_END()/g'                \
-    -e 's/[ \t]*#[ \t]*include[ \t]*[<\"]\(.*\)[>\"]/PPINCLUDE(\"\1\");/g'                \
+    -e 's/[ \t]*#[ \t]*include[ \t]*\"\(.*\)\"/PPINCLUDE(\"\1\");/g'                \
+    -e 's/[ \t]*#[ \t]*include[ \t]*<\(.*\)>/PPINCLUDE(\"<\1>\");/g'                \
     -e 's/[ \t]*#[ \t]*define[ \t]*\(.*\)/PPDEFINE(R"EOF(\1)EOF");/g'                \
     | \
 clang-format \
     | \
 sed \
-    -e 's/ \([ \t]*\)_Pragma(\"\(.*\)\");/#\1pragma \2/g' \
-    -e 's/_Pragma(\"\(.*\)\");/#pragma \1/g'
-#    -e 's/ \([ \t]*\)PPIF_BEGIN(\(.*\))/#\1\2/g'            \
-#    -e 's/PPIF_BEGIN(\(.*\))/#if\1/g'                         \
-#    -e 's/ \([ \t]*\)PPIF_END()/#\1endif/g'              \
-#    -e 's/PPIF_END()/#endif/g'                           \
-#   
+    -e 's/ \([ \t]*\)_Pragma(\"\(.*\)\");/#\1pragma \2/g'       \
+    -e 's/_Pragma(\"\(.*\)\");/#pragma \1/g'                    \
+    -e 's/ \([ \t]*\)PPIF_BEGIN(\"\(.*\)\")/#\1\2/g'            \
+    -e 's/PPIF_BEGIN(\"\(.*\)\")/#\1/g'                         \
+    -e 's/ \([ \t]*\)PPIF_END()/#\1endif/g'                     \
+    -e 's/PPIF_END()/#endif/g'                                  \
+    -e 's/ \([ \t]*\)PPIF_END(\"else\")/#\1else/g'              \
+    -e 's/PPIF_END(\"else\")/\/\*EMPTY LINE\*\//g'
+
