de8e636c4cd732d723d5ebc51c63f0a0aedede27
Thierry Carrard <thierry.carrard@cea.fr>
2024-04-24 13:06:31 +0200

    improving Paraview output to ensure mirror periodic conditions work

--- src/exanb/io/include/exanb/io/write_paraview.h
+++ src/exanb/io/include/exanb/io/write_paraview.h
@@ -133,7 +133,7 @@ namespace exanb
     template<class LDBG, class GridT, class CellsAccesorT, class FieldSelectorT, class... GridFields >
     static inline void write_particles(LDBG& ldbg,
               MPI_Comm comm, const GridT& grid, const CellsAccesorT& cells, const Domain& domain, const std::string& filename, const FieldSelectorT& field_selector,
-              const std::string& compression, bool binary_mode, bool write_box, bool write_external_box, bool write_ghost, const GridFields& ... grid_fields )
+              const std::string& compression, bool binary_mode, bool write_box, bool write_ghost, const GridFields& ... grid_fields )
     {    
       Mat3d xform = domain.xform();
 
@@ -162,7 +162,14 @@ namespace exanb
       {
         std::filesystem::create_directories( filename );
         std::string filename_pvtp = (filename) + ".pvtp";
-        ldbg << "write " << filename_pvtp << std::endl;
+
+        const auto last_slash = filename.rfind('/');
+        std::string subdir = filename;
+        if( last_slash != std::string::npos )
+        {
+          subdir = filename.substr(last_slash+1) + "/";
+        }
+        ldbg << "Write pvtp file '" << filename_pvtp <<"' , subdir = '"<<subdir<<"'"<< std::endl;
 
         std::ofstream file_pvtp(filename_pvtp);
         if( ! file_pvtp.good() )
@@ -174,7 +181,7 @@ namespace exanb
                   << vtk_space_offset_four << "<PPolyData GhostLevel=\"0\">" << std::endl
                   << vtk_space_offset_four << "<PPointData Scalar=\"" ;
         apply_grid_fields( grid, WriteScalarList{file_pvtp,field_selector}, grid_fields... );
-        if(write_ghost) file_pvtp <<", ghost";
+        if(write_ghost) file_pvtp <<", FANTOMAS";
         file_pvtp << "\" Vector=\"" ;
         apply_grid_fields( grid, WriteVectorList{file_pvtp,field_selector}, grid_fields... ); 
         file_pvtp << "\">" << std::endl;
@@ -183,7 +190,7 @@ namespace exanb
           
         if(write_ghost)
         {
-          file_pvtp << vtk_space_offset_six << "<PDataArray type=\"UInt8\" Name=\"ghost\"/>" << std::endl;
+          file_pvtp << vtk_space_offset_six << "<PDataArray type=\"UInt8\" Name=\"FANTOMAS\"/>" << std::endl;
         }
 
         file_pvtp << vtk_space_offset_four << "</PPointData>" << std::endl
@@ -193,15 +200,11 @@ namespace exanb
 
         for(int i=0;i<np; ++i)
         {
-          const auto last_slash = filename.rfind('/');
-          std::string subdir = filename;
-          if( last_slash != std::string::npos )
-          {
-            subdir = filename.substr(last_slash+1) + "/";
-          }
-          const std::string filename_vtp = format_string("%s/%09d.vtp",subdir,i);
+          const std::string filename_vtp = format_string("%s%09d.vtp",subdir,i);
           file_pvtp << vtk_space_offset_four << "<Piece Source=\""<< filename_vtp <<"\"/>" << std::endl;
         }
+
+        //if( write_box ) file_pvtp << vtk_space_offset_four << "<Piece Source=\""<<subdir<<"box.vtp\"/>" << std::endl;
 
         file_pvtp << vtk_space_offset_two << "</PPolyData>" << std::endl << "</VTKFile>" << std::endl;
         file_pvtp.close();
@@ -216,6 +219,8 @@ namespace exanb
       size_t n_cells = grid.number_of_cells();
       size_t nb_particles = grid.number_of_particles();
       if(!write_ghost) { nb_particles -= grid.number_of_ghost_particles(); }
+      ldbg << "Grid: cells="<<grid.number_of_cells() <<", ghost_layers()="<<grid.ghost_layers()<<", ghost partilces="<<grid.number_of_ghost_particles()
+           << ", particles to write = "<<nb_particles << std::endl;
 
       const std::string filename_vtp = format_string("%s/%09d.vtp",filename,rank);
       ldbg << "write " << filename_vtp << std::endl;
@@ -239,17 +244,13 @@ namespace exanb
         if(binary_mode)
         {
           std::vector<uint8_t> sources;
-          for(size_t c=0; c<n_cells;++c)
-          {
-            if(grid.is_ghost_cell(c)) for(size_t pos=0;pos<cells[c].size();++pos) sources.push_back(1);
-            else for(size_t pos=0;pos<cells[c].size();++pos) sources.push_back(0);
-          }
-          file_vtp <<  vtk_space_offset_eight << "<DataArray type=\"UInt8\" Name=\"ghost\" format=\"binary\">"<< std::endl;
+          for(size_t c=0; c<n_cells;++c) { sources.insert( sources.end() , cells[c].size() , uint8_t(grid.is_ghost_cell(c)) ); }
+          file_vtp <<  vtk_space_offset_eight << "<DataArray type=\"UInt8\" Name=\"FANTOMAS\" format=\"binary\">"<< std::endl;
           write_binary_datas(file_vtp, compression_level, sources);
         }
         else
         {
-          file_vtp <<  vtk_space_offset_eight << "<DataArray type=\"UInt8\" Name=\"ghost\" format=\"ascii\">" << std::endl;
+          file_vtp <<  vtk_space_offset_eight << "<DataArray type=\"UInt8\" Name=\"FANTOMAS\" format=\"ascii\">" << std::endl;
           for(size_t c=0; c<n_cells;++c)
           {
             if(grid.is_ghost_cell(c)) for(size_t pos=0;pos<cells[c].size();++pos) file_vtp << ' ' << 1;
@@ -317,136 +318,72 @@ namespace exanb
       // Simulation box
       // TO DO : box will be dependant of the time
       if(write_box && rank==0)
-        {
-          ldbg << "write box" << std::endl;
-          std::stringstream box_name_oss;
-          box_name_oss << filename << "/box.vtp";
-          std::string box_name = box_name_oss.str();
+      {
+        std::string box_name = format_string("%s/box.vtp",filename);
+        ldbg << "write box to "<<box_name << std::endl;
 
-          std::ifstream f(box_name);
-          if(!f.good())
-            {
-              std::ofstream box_vtp;
+        std::ifstream f(box_name);
+        if(!f.good())
+          {
+            std::ofstream box_vtp;
 
-              Vec3d a1 = xform * Vec3d{domain.origin().x, domain.origin().y, domain.origin().z};
-              Vec3d a2 = xform * Vec3d{domain.extent().x, domain.origin().y, domain.origin().z};
-              Vec3d a3 = xform * Vec3d{domain.extent().x, domain.extent().y, domain.origin().z};
-              Vec3d a4 = xform * Vec3d{domain.origin().x, domain.extent().y, domain.origin().z};
-              Vec3d a5 = xform * Vec3d{domain.origin().x, domain.origin().y, domain.extent().z};
-              Vec3d a6 = xform * Vec3d{domain.extent().x, domain.origin().y, domain.extent().z};
-              Vec3d a7 = xform * Vec3d{domain.extent().x, domain.extent().y, domain.extent().z};
-              Vec3d a8 = xform * Vec3d{domain.origin().x, domain.extent().y, domain.extent().z};
+            Vec3d a1 = xform * Vec3d{domain.origin().x, domain.origin().y, domain.origin().z};
+            Vec3d a2 = xform * Vec3d{domain.extent().x, domain.origin().y, domain.origin().z};
+            Vec3d a3 = xform * Vec3d{domain.extent().x, domain.extent().y, domain.origin().z};
+            Vec3d a4 = xform * Vec3d{domain.origin().x, domain.extent().y, domain.origin().z};
+            Vec3d a5 = xform * Vec3d{domain.origin().x, domain.origin().y, domain.extent().z};
+            Vec3d a6 = xform * Vec3d{domain.extent().x, domain.origin().y, domain.extent().z};
+            Vec3d a7 = xform * Vec3d{domain.extent().x, domain.extent().y, domain.extent().z};
+            Vec3d a8 = xform * Vec3d{domain.origin().x, domain.extent().y, domain.extent().z};
 
-              box_vtp.open(box_name);
-              box_vtp << "<?xml version=\"1.0\"?>" << std::endl
-                      << "<VTKFile type=\"PolyData\" version=\"1.0\" byte_order=\"LittleEndian\" >" << std::endl
-                      << vtk_space_offset_two << "<PolyData>" << std::endl
-                      << vtk_space_offset_four << "<Piece NumberOfPoints=\"8\" NumberOfVerts=\"0\" NumberOfLines=\"0\" NumberOfStrips=\"0\" NumberOfPolys=\"6\">" << std::endl
-                      << vtk_space_offset_six << "<Points>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">" << std::endl
-                      << vtk_space_offset_ten
-                      << a1.x << " " << a1.y << " " << a1.z << " "
-                      << a2.x << " " << a2.y << " " << a2.z << " "
-                      << a3.x << " " << a3.y << " " << a3.z << " "
-                      << a4.x << " " << a4.y << " " << a4.z << " "
-                      << a5.x << " " << a5.y << " " << a5.z << " "
-                      << a6.x << " " << a6.y << " " << a6.z << " "
-                      << a7.x << " " << a7.y << " " << a7.z << " "
-                      << a8.x << " " << a8.y << " " << a8.z << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_six << "</Points>" << std::endl
-                      << vtk_space_offset_six << "<Polys>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">" << std::endl
-                      << vtk_space_offset_ten << "0 1 2 3 4 5 6 7 0 1 5 4 2 3 7 6 0 4 7 3 1 2 6 5" << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\"> " << std::endl
-                      << vtk_space_offset_ten << "4 8 12 16 20 24" << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_six << "</Polys>" << std::endl
-                      << vtk_space_offset_four << "</Piece>" << std::endl
-                      << vtk_space_offset_two << "</PolyData>" << std::endl
-                      << "</VTKFile>" << std::endl;
+            box_vtp.open(box_name);
+            box_vtp << "<?xml version=\"1.0\"?>" << std::endl
+                    << "<VTKFile type=\"PolyData\" version=\"1.0\" byte_order=\"LittleEndian\" >" << std::endl
+                    << vtk_space_offset_two << "<PolyData>" << std::endl
+                    << vtk_space_offset_four << "<Piece NumberOfPoints=\"8\" NumberOfVerts=\"0\" NumberOfLines=\"0\" NumberOfStrips=\"0\" NumberOfPolys=\"6\">" << std::endl
+                    << vtk_space_offset_six << "<Points>" << std::endl
+                    << vtk_space_offset_eight << "<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">" << std::endl
+                    << vtk_space_offset_ten
+                    << a1.x << " " << a1.y << " " << a1.z << " "
+                    << a2.x << " " << a2.y << " " << a2.z << " "
+                    << a3.x << " " << a3.y << " " << a3.z << " "
+                    << a4.x << " " << a4.y << " " << a4.z << " "
+                    << a5.x << " " << a5.y << " " << a5.z << " "
+                    << a6.x << " " << a6.y << " " << a6.z << " "
+                    << a7.x << " " << a7.y << " " << a7.z << " "
+                    << a8.x << " " << a8.y << " " << a8.z << std::endl
+                    << vtk_space_offset_eight << "</DataArray>" << std::endl
+                    << vtk_space_offset_six << "</Points>" << std::endl
+                    << vtk_space_offset_six << "<Polys>" << std::endl
+                    << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">" << std::endl
+                    << vtk_space_offset_ten << "0 1 2 3 4 5 6 7 0 1 5 4 2 3 7 6 0 4 7 3 1 2 6 5" << std::endl
+                    << vtk_space_offset_eight << "</DataArray>" << std::endl
+                    << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\"> " << std::endl
+                    << vtk_space_offset_ten << "4 8 12 16 20 24" << std::endl
+                    << vtk_space_offset_eight << "</DataArray>" << std::endl
+                    << vtk_space_offset_six << "</Polys>" << std::endl
+                    << vtk_space_offset_four << "</Piece>" << std::endl
+                    << vtk_space_offset_two << "</PolyData>" << std::endl
+                    << "</VTKFile>" << std::endl;
 
-              box_vtp.close();
-            }
-        }
-      //---------------------------------------------------------------------------------------
+            box_vtp.close();
+          }
+      }
 
-
-
-      //----------------------------------EXTERNAL BOX-----------------------------------------
-      // External box is a box taking inside ghosts
-      // TO DO : box will be dependant of the time
-      if(write_external_box && rank==0)
-        {
-          ldbg << "write external box" << std::endl;
-          std::stringstream box_name_oss;
-          box_name_oss << filename << "_external_box.vtp";
-          std::string box_name = box_name_oss.str();
-
-          std::ifstream f(box_name);
-
-          size_t glayers = grid.ghost_layers();
-
-          // origin of the box
-          // Depend if their are periodics conditions
-          double o_x = domain.periodic_boundary_x() ? domain.origin().x - domain.cell_size()*glayers : domain.origin().x;
-          double o_y = domain.periodic_boundary_y() ? domain.origin().y - domain.cell_size()*glayers : domain.origin().y;
-          double o_z = domain.periodic_boundary_z() ? domain.origin().z - domain.cell_size()*glayers : domain.origin().z;
-
-          // end of the box
-          double e_x = domain.periodic_boundary_x() ? domain.extent().x + domain.cell_size()*glayers : domain.extent().x;
-          double e_y = domain.periodic_boundary_y() ? domain.extent().y + domain.cell_size()*glayers : domain.extent().y;
-          double e_z = domain.periodic_boundary_z() ? domain.extent().z + domain.cell_size()*glayers : domain.extent().z;
-
-          Vec3d a1 = xform * Vec3d{o_x, o_y, o_z};
-          Vec3d a2 = xform * Vec3d{e_x, o_y, o_z};
-          Vec3d a3 = xform * Vec3d{e_x, e_y, o_z};
-          Vec3d a4 = xform * Vec3d{o_x, e_y, o_z};
-          Vec3d a5 = xform * Vec3d{o_x, o_y, e_z};
-          Vec3d a6 = xform * Vec3d{e_x, o_y, e_z};
-          Vec3d a7 = xform * Vec3d{e_x, e_y, e_z};
-          Vec3d a8 = xform * Vec3d{o_x, e_y, e_z};
-
-          if(!f.good())
-            {
-              std::ofstream box_vtp;
-              box_vtp.open(box_name);
-              box_vtp << "<?xml version=\"1.0\"?>" << std::endl
-                      << "<VTKFile type=\"PolyData\" version=\"1.0\" byte_order=\"LittleEndian\" >" << std::endl
-                      << vtk_space_offset_two << "<PolyData>" << std::endl
-                      << vtk_space_offset_four << "<Piece NumberOfPoints=\"8\" NumberOfVerts=\"0\" NumberOfLines=\"0\" NumberOfStrips=\"0\" NumberOfPolys=\"6\">" << std::endl
-                      << vtk_space_offset_six << "<Points>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Float32\" NumberOfComponents=\"3\" format=\"ascii\">" << std::endl
-                      << vtk_space_offset_ten
-                      << a1.x << " " << a1.y << " " << a1.z << " "
-                      << a2.x << " " << a2.y << " " << a2.z << " "
-                      << a3.x << " " << a3.y << " " << a3.z << " "
-                      << a4.x << " " << a4.y << " " << a4.z << " "
-                      << a5.x << " " << a5.y << " " << a5.z << " "
-                      << a6.x << " " << a6.y << " " << a6.z << " "
-                      << a7.x << " " << a7.y << " " << a7.z << " "
-                      << a8.x << " " << a8.y << " " << a8.z << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_six << "</Points>" << std::endl
-                      << vtk_space_offset_six << "<Polys>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"connectivity\" format=\"ascii\">" << std::endl
-                      << vtk_space_offset_ten << "0 1 2 3 4 5 6 7 0 1 5 4 2 3 7 6 0 4 7 3 1 2 6 5" << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_eight << "<DataArray type=\"Int32\" Name=\"offsets\" format=\"ascii\"> " << std::endl
-                      << vtk_space_offset_ten << "4 8 12 16 20 24" << std::endl
-                      << vtk_space_offset_eight << "</DataArray>" << std::endl
-                      << vtk_space_offset_six << "</Polys>" << std::endl
-                      << vtk_space_offset_four << "</Piece>" << std::endl
-                      << vtk_space_offset_two << "</PolyData>" << std::endl
-                      << "</VTKFile>" << std::endl;
-
-              box_vtp.close();
-            }
-        }
       //---------------------------------------------------------------------------------------
       ldbg << "write END" << std::endl;
     }
+    
+    template<class LDBG, class GridT, class CellsAccesorT, class FieldSelectorT, class... GridFields >
+    [[deprecated]]
+    static inline void write_particles(LDBG& ldbg,
+              MPI_Comm comm, const GridT& grid, const CellsAccesorT& cells, const Domain& domain, const std::string& filename, const FieldSelectorT& field_selector,
+              const std::string& compression, bool binary_mode, bool write_box, bool write_box_external, bool write_ghost, const GridFields& ... grid_fields )
+    {
+      lerr << "DEPRECATED: write_box_external additional parameter is deprecated, please call paraview's writer method 'write_particles' without this parameter"<<std::endl;
+      write_particles(ldbg,comm,grid,cells,domain,filename,field_selector,compression,binary_mode,write_box,write_ghost,grid_fields...);
+    }
+
   }
 
 }
