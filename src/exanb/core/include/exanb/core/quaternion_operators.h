47b2e8f018a7b2ddd4787ed7158e86c065f36cf0
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2024-07-13 14:51:20 +0200

    add a randomize function (quaternion)

--- src/exanb/core/include/exanb/core/quaternion_operators.h
+++ src/exanb/core/include/exanb/core/quaternion_operators.h
@@ -98,73 +98,75 @@ namespace exanb
   ONIKA_HOST_DEVICE_FUNC inline double norm(const Quaternion& q)
   {
     return ( sqrt( q.w*q.w + q.x*q.x + q.y*q.y + q.z*q.z ) );
-  }
-  
-  ONIKA_HOST_DEVICE_FUNC inline Quaternion normalize(const Quaternion& q)
-  {
-    return ( q / norm( q ) );
-  }
+	}
 
-  ONIKA_HOST_DEVICE_FUNC inline Quaternion get_conjugated(const Quaternion& q)
-  {
-    Quaternion ret;
-    ret.w = q.w;
-    ret.x = -q.x;
-    ret.y = -q.y;
-    ret.z = -q.z;
-    return ret;
-  }
-  
-  /// @brief Return the time derivative of the quaternion \f$ \dot{q} = \frac{1}{2} \omega q\f$
-  ONIKA_HOST_DEVICE_FUNC inline Quaternion dot(const Quaternion& q, const Vec3d& omega)
-  {
-    Quaternion ret;
-    ret.x = 0.5 * (omega.y * q.z - omega.z * q.y + omega.x * q.w);
-    ret.y = 0.5 * (omega.z * q.x - omega.x * q.z + omega.y * q.w);
-    ret.z = 0.5 * (omega.x * q.y - omega.y * q.x + omega.z * q.w);
-    ret.w = -0.5 * (exanb::dot(omega,Vec3d{q.x,q.y,q.z})); // real dot product
-    return ret;
-  }
-  
-  /// @brief Return d2Q/dt2
-  ONIKA_HOST_DEVICE_FUNC inline Quaternion ddot (const Quaternion& q, const Vec3d& vrot, const Vec3d& arot)
-  {
-/*
-    	  Quaternion ret;
-    double omega2_2 = 0.5 * exanb::dot(vrot,vrot);
-    auto xyz = Vec3d{q.x,q.y,q.z};
-    auto tmp = 0.5 * (q.w * arot + exanb::cross(arot, xyz) -
-                        omega2_2 * xyz);
-    ret.x = tmp.x;
-    ret.y = tmp.y;
-    ret.z = tmp.z;
-    ret.w = -0.5 * (exanb::dot(arot , xyz) + omega2_2) ;
+	ONIKA_HOST_DEVICE_FUNC inline Quaternion normalize(const Quaternion& q)
+	{
+		return ( q / norm( q ) );
+	}
 
-    return ret;
-*/
+	ONIKA_HOST_DEVICE_FUNC inline Quaternion get_conjugated(const Quaternion& q)
+	{
+		Quaternion ret;
+		ret.w = q.w;
+		ret.x = -q.x;
+		ret.y = -q.y;
+		ret.z = -q.z;
+		return ret;
+	}
 
-    auto xyz = Vec3d{q.x,q.y,q.z};
-    auto tmp1 = q.w * arot + cross(arot, xyz);
-    auto tmp2 = - dot(arot , xyz);
-    //quat q1(s * domega + cross(domega, v), -domega * v); // remark: ctor is quat(v, s)
-    Quaternion q1 = {tmp1.x, tmp1.y, tmp1.z, tmp2}; 
-    Vec3d c = cross(vrot, xyz);
-    //quat q2(-(omega * v) * omega + cross(omega, s * omega + c), omega * (s * omega + c));
-    tmp1 = - dot(vrot, xyz) * vrot + cross (vrot, q.w*vrot + c);
-    tmp2 = dot(vrot , (q.w * vrot + c)); 
-    Quaternion q2 = {tmp1.x, tmp1.y, tmp1.z, tmp2};
-    q1 += (q2 = q2 * 0.5);
-    q1 = q1 * 0.5;
-    return q1;
-  }
+	/// @brief Return the time derivative of the quaternion \f$ \dot{q} = \frac{1}{2} \omega q\f$
+	ONIKA_HOST_DEVICE_FUNC inline Quaternion dot(const Quaternion& q, const Vec3d& omega)
+	{
+		Quaternion ret;
+		ret.x = 0.5 * (omega.y * q.z - omega.z * q.y + omega.x * q.w);
+		ret.y = 0.5 * (omega.z * q.x - omega.x * q.z + omega.y * q.w);
+		ret.z = 0.5 * (omega.x * q.y - omega.y * q.x + omega.z * q.w);
+		ret.w = -0.5 * (exanb::dot(omega,Vec3d{q.x,q.y,q.z})); // real dot product
+		return ret;
+	}
 
-  // Rotation of a vector by a quat
-  ONIKA_HOST_DEVICE_FUNC inline Vec3d operator * (const Quaternion& q, const Vec3d& v)
-  {
-    const auto xyz = Vec3d{q.x,q.y,q.z};
-    auto qv = cross(xyz, v);
-    auto qqv = cross(xyz, qv);
-    return v + qv*2.0*q.w + qqv*2.0;
-  }
+	/// @brief Return d2Q/dt2
+	ONIKA_HOST_DEVICE_FUNC inline Quaternion ddot (const Quaternion& q, const Vec3d& vrot, const Vec3d& arot)
+	{
+		auto xyz = Vec3d{q.x,q.y,q.z};
+		auto tmp1 = q.w * arot + cross(arot, xyz);
+		auto tmp2 = - dot(arot , xyz);
+		//quat q1(s * domega + cross(domega, v), -domega * v); // remark: ctor is quat(v, s)
+		Quaternion q1 = {tmp1.x, tmp1.y, tmp1.z, tmp2}; 
+		Vec3d c = cross(vrot, xyz);
+		//quat q2(-(omega * v) * omega + cross(omega, s * omega + c), omega * (s * omega + c));
+		tmp1 = - dot(vrot, xyz) * vrot + cross (vrot, q.w*vrot + c);
+		tmp2 = dot(vrot , (q.w * vrot + c)); 
+		Quaternion q2 = {tmp1.x, tmp1.y, tmp1.z, tmp2};
+		q1 += (q2 = q2 * 0.5);
+		q1 = q1 * 0.5;
+		return q1;
+	}
+
+	// Rotation of a vector by a quat
+	ONIKA_HOST_DEVICE_FUNC inline Vec3d operator * (const Quaternion& q, const Vec3d& v)
+	{
+		const auto xyz = Vec3d{q.x,q.y,q.z};
+		auto qv = cross(xyz, v);
+		auto qqv = cross(xyz, qv);
+		return v + qv*2.0*q.w + qqv*2.0;
+	}
+
+	inline void randomize(bool seedTime = false) 
+	{
+		// @see http://hub.jmonkeyengine.org/t/random-quaternions/8431
+		static std::default_random_engine engine;
+		if (seedTime == true) engine.seed(std::chrono::system_clock::now().time_since_epoch().count());
+		static std::uniform_real_distribution<double> distrib(-1.0, 1.0);
+		double sum = 0.0;
+		q.w = distrib(engine);
+		sum += s * s;
+		q.x = sqrt(1 - sum) * distrib(engine);
+		sum += v.x * v.x;
+		q.y = sqrt(1 - sum) * distrib(engine);
+		sum += v.y * v.y;
+		q.z = sqrt(1 - sum) * (distrib(engine) < 0.0 ? -1.0 : 1.0);
+	}
 }
 
