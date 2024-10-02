b295825354ac507c7aba8dc05a7392a4a71f7136
Thierry Carrard <thierry.carrard@cea.fr>
2024-10-02 09:21:26 +0200

    added tutorial component with exemples to illustrate documentation with working exemples

---
+++ src/exanb/tutorial/array2d.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <onika/cuda/cuda.h>
+#include <onika/memory/allocator.h>
+#include <onika/cuda/stl_adaptors.h>
+
+// a specific namespace for our application space
+namespace tutorial
+{
+  using namespace exanb;
+  
+  struct Array2D
+  {
+    onika::memory::CudaMMVector<double> m_data;
+    size_t m_rows = 0;              // number of rows in our array
+    size_t m_columns = 0;           // number of columns in our array
+    inline void resize(size_t r, size_t c) { m_rows=r; m_columns=c; m_data.resize(r*c); }
+    ONIKA_HOST_DEVICE_FUNC inline double const * data() const { return onika::cuda::vector_data( m_data ); }
+    ONIKA_HOST_DEVICE_FUNC inline double * data() { return onika::cuda::vector_data( m_data ); }
+    ONIKA_HOST_DEVICE_FUNC inline double const * operator [] (size_t row) const { return data() + ( m_columns * row ); }
+    ONIKA_HOST_DEVICE_FUNC inline double * operator [] (size_t row) { return data() + ( m_columns * row ); }
+    ONIKA_HOST_DEVICE_FUNC inline size_t columns() const { return m_columns; }
+    ONIKA_HOST_DEVICE_FUNC inline size_t rows() const { return m_rows; }
+  };  
+
+  struct Array2DReference
+  {
+    double * const m_data_ptr = nullptr; // our 2D array
+    size_t const m_rows = 0;              // number of rows in our array
+    size_t const m_columns = 0;           // number of columns in our array
+    ONIKA_HOST_DEVICE_FUNC inline Array2DReference(Array2DReference && a) = default;
+    ONIKA_HOST_DEVICE_FUNC inline Array2DReference(const Array2DReference& a) : m_data_ptr(a.m_data_ptr) , m_rows(a.m_rows) , m_columns(a.m_columns) {}
+    ONIKA_HOST_DEVICE_FUNC inline Array2DReference(Array2D& a) : m_data_ptr(a.data()) , m_rows(a.rows()) , m_columns(a.columns()) {}
+    ONIKA_HOST_DEVICE_FUNC inline double * operator [] (size_t row) const { return m_data_ptr + ( m_columns * row ); }
+    ONIKA_HOST_DEVICE_FUNC inline size_t columns() const { return m_columns; }
+    ONIKA_HOST_DEVICE_FUNC inline size_t rows() const { return m_rows; }
+  };  
+}
+
---
+++ src/exanb/tutorial/synchronous_block_parallel.cpp
@@ -0,0 +1,46 @@
+#include <exanb/core/operator.h>
+#include <exanb/core/operator_slot.h>
+#include <exanb/core/operator_factory.h>
+#include <onika/parallel/block_parallel_for.h>
+
+#include "array2d.h"
+#include "block_parallel_value_add_functor.h"
+
+namespace tutorial
+{
+  using namespace exanb;
+
+  class SynchronousBlockParallelSample : public OperatorNode
+  {
+    ADD_SLOT(Array2D, my_array, INPUT_OUTPUT, REQUIRED);
+    ADD_SLOT(double, my_value, INPUT, 1.0);
+    
+    public:
+    
+    inline void execute() override final
+    {
+      if( my_array->rows() == 0 || my_array->columns() == 0 )
+      {
+        my_array->resize( 1024 , 1024 );
+      }
+    
+      BlockParallelValueAddFunctor value_add_func = { *my_array // refernce our data array through its pointer and size
+                                                    , *my_value // value to add to the elements of the array
+                                                    };
+                             
+      // Launching the parallel operation, which can execute on GPU if the execution context allows
+      onika::parallel::block_parallel_for( my_array->rows()             // number of iterations, parallelize at the first level over rows
+                                         , value_add_func               // the function to call in parallel
+                                         , parallel_execution_context() // returns the parallel execution context associated with this component
+                                         );
+    }
+  };
+
+  // === register factories ===
+  CONSTRUCTOR_FUNCTION
+  {
+   OperatorNodeFactory::instance()->register_factory( "synchronous_block_parallel", make_simple_operator< SynchronousBlockParallelSample > );
+  }
+
+}
+
