a3a489b45cd76e6e6931c34c47589274495cf5fc
Thierry Carrard <thierry.carrard@cea.fr>
2025-03-01 07:26:59 +0100

    updated paraview programmable filter script

--- contribs/paraview/cc_label_programmable_filter.py
+++ contribs/paraview/cc_label_programmable_filter.py
@@ -11,10 +11,10 @@ cc_table = np.genfromtxt("cc_table.csv" , dtype=None, names=True, delimiter=';',
 cc_count = cc_table["count"]
 cc_rank = cc_table["rank"]
 N = cc_label.size
-cc_volume = np.array( [] , dtype=np.uint32 )
+cc_volume = np.array( [] , dtype=np.int32 )
 cc_volume.resize(N)
 cc_volume.reshape([N])
-cc_procid = np.array( [] , dtype=np.uint32 )
+cc_procid = np.array( [] , dtype=np.int32 )
 cc_procid.resize(N)
 cc_procid.reshape([N])
 for i in range(N):
@@ -26,7 +26,35 @@ for i in range(N):
       cc_volume[i] = cc_count[label_idx]
       cc_procid[i] = cc_rank[label_idx]
     else:
-      cc_volume[i] = 0
+      cc_volume[i] = -1
+      cc_procid[i] = -1
+output.PointData.append( cc_volume , "cc_volume" )
+output.PointData.append( cc_procid , "cc_procid" )
+
+# alternative version robust to label values errors
+import numpy as np
+input0 = inputs[0]
+cc_label = input0.PointData["cc_label"]
+cc_table = np.genfromtxt("cc_table.csv" , dtype=None, names=True, delimiter=';', autostrip=True)
+cc_count = cc_table["count"]
+cc_rank = cc_table["rank"]
+N = cc_label.size
+cc_volume = np.array( [] , dtype=np.int32 )
+cc_volume.resize(N)
+cc_volume.reshape([N])
+cc_procid = np.array( [] , dtype=np.int32 )
+cc_procid.resize(N)
+cc_procid.reshape([N])
+assert cc_count.size == cc_rank.size , "Table fields size mismatch"
+for i in range(N):
+    assert i < cc_volume.size , "index %d out of cc_volume bounds (%d)" % (i,cc_volume.size)
+    label_idx = int(cc_label[i])
+    if label_idx >= 0 and label_idx < cc_count.size:
+      cc_volume[i] = cc_count[label_idx]
+      cc_procid[i] = cc_rank[label_idx]
+    else:
+      cc_volume[i] = -1
+      cc_procid[i] = -1
 output.PointData.append( cc_volume , "cc_volume" )
 output.PointData.append( cc_procid , "cc_procid" )
 
