a56e8312535d8e07d6ecd3e29894a932d825457f
Thierry Carrard <thierry.carrard@cea.fr>
2025-05-06 19:25:22 +0200

    fixed bug in box/triangle intersection

--- src/mesh/include/exanb/mesh/triangle_math.h
+++ src/mesh/include/exanb/mesh/triangle_math.h
@@ -115,100 +115,79 @@ namespace exanb
     return { proj_p, d, u, v, w, N , d < 0 , ( u>=0 && u<=1 && v>=0 && v<=1 && w>=0 && w<=1 ) };
   }
 
+  
+  ONIKA_HOST_DEVICE_FUNC
+  inline int plane_polygon_clip(const Vec3d& plane_n, const double plane_d, const onika::math::Vec3d * in_verts , int n , onika::math::Vec3d * out_verts , int max_n)
+  {
+    using onika::math::dot;
+    int n_out_verts = 0;
+
+    if( n <= 0 ) return n;
+    for(int i=0;i<n;i++)
+    {
+      const auto e0 = in_verts[i];
+      const auto e1 = in_verts[(i+1)%n];
+      const auto d0 = dot( plane_n , e0 ) + plane_d;
+      const auto d1 = dot( plane_n , e1 ) + plane_d;
+      if( d0 >= 0 )
+      {
+        assert( n_out_verts < max_n );
+        out_verts[n_out_verts++] = e0;
+      }
+      if( d0*d1 < 0 )
+      {
+        assert( n_out_verts < max_n );
+        const auto delta_d = d1 - d0;
+        const auto d01_norm_factor = ( d0 != 0 || d1 != 0 ) ? ( abs(d0) + abs(d1) ) : 1.0 ;
+        const auto edge_u = abs(delta_d/d01_norm_factor)>1e-12 ? ( -d0 / delta_d ) : 0.5;
+        out_verts[n_out_verts++] = e0 + edge_u * ( e1 - e0 );
+      }
+    }
+    return n_out_verts;
+  }
+
   ONIKA_HOST_DEVICE_FUNC
   inline bool intersect(const onika::math::AABB & box, const Triangle& tri)
   {
     using onika::cuda::min;
     using onika::cuda::max;
     using onika::math::Vec3d;
+    static constexpr size_t MAX_POLY_VERTS = 12;
     
-	  const auto tri_bounds = bounds( tri );
+    Vec3d poly_buf_a[MAX_POLY_VERTS] = { tri[0] , tri[1] , tri[2] , };
+    auto * poly_a = poly_buf_a;
+    int n_verts = 3;
+    Vec3d poly_buf_b[MAX_POLY_VERTS];
+    auto * poly_b = poly_buf_b;
+    
+    const double box_min[3] = { box.bmin.x , box.bmin.y , box.bmin.z };
+    const double box_max[3] = { box.bmax.x , box.bmax.y , box.bmax.z };
 
-	  if( is_empty( intersection(box,tri_bounds) ) ) return false;
+    for(int i=0;i<6;i++)
+    {
+      const int axis = i/2;
+      const bool lower_side = ( (i%2) == 0 );
+      double plane_vec[3] = { 0 , 0 , 0 };
+      plane_vec[ axis ] = lower_side ? 1.0 : -1.0;
+      const Vec3d plane_n = { plane_vec[0] , plane_vec[1] , plane_vec[2] };
+      const double plane_d = lower_side ? - box_min[axis] : box_max[axis];
+      n_verts = plane_polygon_clip( plane_n, plane_d, poly_a, n_verts, poly_b, MAX_POLY_VERTS );
+      if( n_verts == 0 ) return false;
+      auto * tmp = poly_b; poly_b = poly_a; poly_a = tmp;
+    }
 
-	  // triangle-normal and edges
-	  const auto n = normal( tri );
-	  const auto v0 = tri[0];
-	  const auto v1 = tri[1];
-	  const auto v2 = tri[2];
-	  const auto edge0 = v1 - v0;
-	  const auto edge1 = v2 - v1;
-	  const auto edge2 = v0 - v2;
-
-	  // p & delta-p
-	  const auto p  = tri_bounds.bmin;
-	  const auto dp = tri_bounds.bmax - p;
-
-	  // test for triangle-plane/box overlap
-	  Vec3d c = { n.x > 0. ? dp.x : 0., n.y > 0. ? dp.y : 0., n.z > 0. ? dp.z : 0. };
-
-	  const auto d1 = dot(n, c - v0);
-	  const auto d2 = dot(n, dp - c - v0);
-
-	  if( (dot(n, p) + d1) * (dot(n, p) + d2) > 0. ) return false;
-
-	  // xy-plane projection-overlap
-	  const auto xym = (n.z < 0. ? -1. : 1.);
-	  const auto ne0xy = Vec3d{-edge0.y, edge0.x, 0.} * xym;
-	  const auto ne1xy = Vec3d{-edge1.y, edge1.x, 0.} * xym;
-	  const auto ne2xy = Vec3d{-edge2.y, edge2.x, 0.} * xym;
-
-	  const auto v0xy = Vec3d{v0.x, v0.y, 0.};
-	  const auto v1xy = Vec3d{v1.x, v1.y, 0.};
-	  const auto v2xy = Vec3d{v2.x, v2.y, 0.};
-
-	  const auto de0xy = -dot(ne0xy, v0xy) + max(0., dp.x * ne0xy.x) + max(0., dp.y * ne0xy.y);
-	  const auto de1xy = -dot(ne1xy, v1xy) + max(0., dp.x * ne1xy.x) + max(0., dp.y * ne1xy.y);
-	  const auto de2xy = -dot(ne2xy, v2xy) + max(0., dp.x * ne2xy.x) + max(0., dp.y * ne2xy.y);
-
-	  const Vec3d pxy = { p.x, p.y, 0. };
-
-	  if( (dot(ne0xy, pxy) + de0xy) < 0. || (dot(ne1xy, pxy) + de1xy) < 0. || (dot(ne2xy, pxy) + de2xy) < 0. ) return false;
-
-	  // yz-plane projection overlap
-	  const auto yzm = (n.x < 0. ? -1. : 1.);
-	  const auto ne0yz = Vec3d{-edge0.z, edge0.y, 0. } * yzm;
-	  const auto ne1yz = Vec3d{-edge1.z, edge1.y, 0. } * yzm;
-	  const auto ne2yz = Vec3d{-edge2.z, edge2.y, 0. } * yzm;
-
-	  const Vec3d v0yz = { v0.y, v0.z, 0. };
-	  const Vec3d v1yz = { v1.y, v1.z, 0. };
-	  const Vec3d v2yz = { v2.y, v2.z, 0. };
-
-	  const auto de0yz = -dot(ne0yz, v0yz) + max(0., dp.y * ne0yz.x) + max(0., dp.z * ne0yz.y);
-	  const auto de1yz = -dot(ne1yz, v1yz) + max(0., dp.y * ne1yz.x) + max(0., dp.z * ne1yz.y);
-	  const auto de2yz = -dot(ne2yz, v2yz) + max(0., dp.y * ne2yz.x) + max(0., dp.z * ne2yz.y);
-
-	  const Vec3d pyz = { p.y, p.z, 0. };
-
-	  if( (dot(ne0yz, pyz) + de0yz) < 0. || (dot(ne1yz, pyz) + de1yz) < 0. || (dot(ne2yz, pyz) + de2yz) < 0. ) return false;
-
-	  // zx-plane projection overlap
-	  const auto zxm = (n.y < 0. ? -1. : 1.);
-	  const auto ne0zx = Vec3d{-edge0.x, edge0.z, 0.} * zxm;
-	  const auto ne1zx = Vec3d{-edge1.x, edge1.z, 0.} * zxm;
-	  const auto ne2zx = Vec3d{-edge2.x, edge2.z, 0.} * zxm;
-
-	  const Vec3d v0zx = {v0.z, v0.x, 0.};
-	  const Vec3d v1zx = {v1.z, v1.x, 0.};
-	  const Vec3d v2zx = {v2.z, v2.x, 0.};
-
-	  const auto de0zx = -dot(ne0zx, v0zx) + max(0., dp.y * ne0zx.x) + max(0., dp.z * ne0zx.y);
-	  const auto de1zx = -dot(ne1zx, v1zx) + max(0., dp.y * ne1zx.x) + max(0., dp.z * ne1zx.y);
-	  const auto de2zx = -dot(ne2zx, v2zx) + max(0., dp.y * ne2zx.x) + max(0., dp.z * ne2zx.y);
-
-	  const Vec3d pzx = { p.z, p.x, 0. };
-
-	  if( (dot(ne0zx, pzx) + de0zx) < 0. || (dot(ne1zx, pzx) + de1zx) < 0. || (dot(ne2zx, pzx) + de2zx) < 0. ) return false;
-
+    //printf("clipped polygon has %d vertices\n",n_verts);
 	  return true;
   }
 
   struct TriangleEdgeIntersection
   {
     Vec3d m_intersection;
+    double m_typical_length;
     double m_tri_u, m_tri_v, m_tri_w;
     double m_edge_u;
+    bool m_plane_edge_intersect;
+    bool m_inside_trianle;
     bool m_intersect;
   };
 
@@ -218,21 +197,37 @@ namespace exanb
     using onika::math::norm;
     using onika::math::cross;
     using onika::math::dot;
-    
+    using onika::cuda::max;
+    using onika::cuda::min;
+
     // triangle unit normal vector
     const auto plane_n = normal( t );
+
+    const Vec3d box_min = { min( min(min(t[0].x,t[1].x),t[2].x) , min(e[0].x,e[1].x) )
+                          , min( min(min(t[0].y,t[1].y),t[2].y) , min(e[0].y,e[1].y) )
+                          , min( min(min(t[0].z,t[1].z),t[2].z) , min(e[0].z,e[1].z) ) };
+    const Vec3d box_max = { max( max(max(t[0].x,t[1].x),t[2].x) , max(e[0].x,e[1].x) )
+                          , max( max(max(t[0].y,t[1].y),t[2].y) , max(e[0].y,e[1].y) )
+                          , max( max(max(t[0].z,t[1].z),t[2].z) , max(e[0].z,e[1].z) ) };
+    const auto typical_length = norm( box_max - box_min );
+    if( typical_length == 0.0 ) return { t[0]+plane_n, 1.0, -1,-1,-1, -1, false, false, false };
+
     const auto plane_d = - dot( plane_n , t[0] );
     
     // compute triangle's plane / edge intersection
     const auto d0 = dot( plane_n , e[0] ) + plane_d;
     const auto d1 = dot( plane_n , e[1] ) + plane_d;
     const auto delta_d = d1 - d0;
-    const auto edge_u = std::abs(delta_d)>1e-12 ? -d0 / delta_d : 0;
+    const auto d01_norm_factor = ( d0 != 0 || d1 != 0 ) ? ( abs(d0) + abs(d1) ) : 1.0 ;
+    const auto edge_u = std::abs(delta_d/d01_norm_factor)>1e-12 ? ( -d0 / delta_d ) : 0.5;
     
     const auto plane_intersection = e[0] + edge_u * ( e[1] - e[0] ); 
-    const auto plane_intersection_dist = dot( plane_n , plane_intersection ) + plane_d;        
+    const auto plane_intersection_dist = dot( plane_n , plane_intersection ) + plane_d;
     const auto [ u , v , w ] = triangle_point_barycentric_coords( t , plane_intersection );
-    return { plane_intersection , u,v,w, edge_u , std::abs(plane_intersection_dist)<1e-9 && edge_u>=0 && edge_u<=1 && u>=0 && u<=1 && v>=0 && v<=1 && w>=0 && w<=1 };
+    
+    const bool plane_edge_intersect = ( edge_u>=0 ) && ( edge_u<=1 ) && ( std::abs(plane_intersection_dist/typical_length) < 1e-9 );
+    const bool inside_trianle = ( u>=0 ) && ( u<=1 ) && ( v>=0 ) && ( v<=1 ) && ( w>=0 ) && ( w<=1 );
+    return { plane_intersection, typical_length, u,v,w, edge_u, plane_edge_intersect, inside_trianle, plane_edge_intersect && inside_trianle };
   }
 
 }
@@ -244,7 +239,7 @@ namespace exanb
 #include <random>
 #include <onika/test/unit_test.h>
 
-ONIKA_UNIT_TEST(triangle_math)
+ONIKA_UNIT_TEST(project_triangle_point)
 {
   using namespace onika;
   using namespace onika::math;
@@ -254,6 +249,7 @@ ONIKA_UNIT_TEST(triangle_math)
   std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_real_distribution<double> ud( -10.0 , 10.0 );
 
+# pragma omp parallel for schedule(static)
   for(int i=0;i<1000000;i++)
   {
     const Triangle t = { Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)}  , Vec3d{ud(gen),ud(gen),ud(gen)} };
@@ -277,25 +273,88 @@ ONIKA_UNIT_TEST(triangle_math)
       ONIKA_TEST_ASSERT( std::abs( dot( p - bary , proj.m_normal ) - proj.m_dist ) < 1.e-9 );
     }
   }
+}
 
+ONIKA_UNIT_TEST(triangle_edge_intersection)
+{
+  using namespace onika;
+  using namespace onika::math;
+  using namespace exanb;
+
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<double> ud( -10.0 , 10.0 );
+
+# pragma omp parallel for schedule(static)
   for(int i=0;i<1000000;i++)
   {
     const Triangle t = { Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)} };
     const Edge e = { Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)} };
-    const auto inter = triangle_edge_intersection( t, e );
-    const auto edge_bary = e[0] + ( inter.m_edge_u * (e[1] - e[0]) );
-    const auto tri_bary = inter.m_tri_u * t[0] + inter.m_tri_v * t[1] + inter.m_tri_w * t[2];
+    const auto inter = triangle_edge_intersection( t, e );    
     const auto plane_n = normal( t );
     const auto plane_d = - dot( t[0] , plane_n );
-
     if( inter.m_intersect )
     {
+      const auto edge_bary = e[0] + ( inter.m_edge_u * (e[1] - e[0]) );
+      const auto tri_bary = inter.m_tri_u * t[0] + inter.m_tri_v * t[1] + inter.m_tri_w * t[2];
+      ONIKA_TEST_ASSERT( inter.m_plane_edge_intersect && inter.m_inside_trianle );
       ONIKA_TEST_ASSERT( (inter.m_tri_u+inter.m_tri_v+inter.m_tri_w-1) < 1e-12 );
       ONIKA_TEST_ASSERT( norm( inter.m_intersection - edge_bary ) < 1e-9 );
       ONIKA_TEST_ASSERT( norm( inter.m_intersection - tri_bary ) < 1e-9 );
       ONIKA_TEST_ASSERT( dot( plane_n , inter.m_intersection ) + plane_d < 1e-9 );
     }
-
+    else
+    {
+      ONIKA_TEST_ASSERT( ! inter.m_plane_edge_intersect || ! inter.m_inside_trianle );      
+      if( inter.m_plane_edge_intersect )
+      {
+        ONIKA_TEST_ASSERT( inter.m_tri_u<0 || inter.m_tri_u>1 || inter.m_tri_v<0 || inter.m_tri_v>1 || inter.m_tri_w<0 || inter.m_tri_w>1 );
+      }
+      if( inter.m_inside_trianle )
+      {
+        ONIKA_TEST_ASSERT( inter.m_edge_u<0 || inter.m_edge_u>1 || ( ( dot( plane_n , inter.m_intersection ) + plane_d ) / inter.m_typical_length ) > 1e-9 );
+      }
+    }
   }
+}
 
+ONIKA_UNIT_TEST(triangle_box_intersection)
+{
+  using namespace onika;
+  using namespace onika::math;
+  using namespace exanb;
+
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<double> ud( -10.0 , 10.0 );
+
+# pragma omp parallel for schedule(static)
+  for(int i=0;i<100000;i++)
+  {
+    const Triangle t = { Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)} , Vec3d{ud(gen),ud(gen),ud(gen)} };
+    const Vec3d p1 = {ud(gen),ud(gen),ud(gen)};
+    const Vec3d p2 = {ud(gen),ud(gen),ud(gen)};
+    const AABB box = { Vec3d{ std::min(p1.x,p2.x) , std::min(p1.y,p2.y) , std::min(p1.z,p2.z) } , Vec3d{ std::max(p1.x,p2.x) , std::max(p1.y,p2.y) , std::max(p1.z,p2.z) } };
+    
+    const AABB all_bounds = extend(box,bounds(t));
+    const auto bounds_length = norm( all_bounds.bmax - all_bounds.bmin );
+    ONIKA_TEST_ASSERT( bounds_length > 0 );
+        
+    double min_dist = 999999.0;
+    for(double u = 0.0 ;  u   <= 1.0 ; u += 0.001 )
+    for(double v = 0.0 ; (u+v)<= 1.0 ; v += 0.001 )
+    {
+      const double w = 1.0 - u - v ;
+      //ONIKA_TEST_ASSERT( std::abs( u+v+w - 1 ) < 1e-12 );
+      const auto p = t[0]*u + t[1]*v + t[2]*w;
+      const auto dist = min_distance_between(p,box);
+      if( dist < min_dist )
+      {
+        min_dist = dist;
+      }
+    }
+    
+    const bool inter = intersect(box,t);
+    ONIKA_TEST_ASSERT( ( inter && (min_dist/bounds_length<1e-3) ) || ( !inter && (min_dist>0) ) );
+  }
 }
