ebc60b0cc6d797fab6d7479631212e2a54148cad
Thierry Carrard <thierry.carrard@cea.fr>
2025-07-30 08:30:05 +0200

    added operator to build cell lists that depend on neighbor process communications

---
+++ src/mpi/update_ghosts_cell_lists.cpp
@@ -0,0 +1,55 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+#include <onika/scg/operator.h>
+#include <onika/scg/operator_slot.h>
+#include <onika/scg/operator_factory.h>
+#include <onika/log.h>
+
+#include <exanb/mpi/ghosts_comm_scheme.h>
+
+namespace exanb
+{
+  
+  struct GhostUpdateCellLists
+  {
+    ghost_sender[]...
+  };
+  
+  struct UpdateGhostCellListsNode : public OperatorNode
+  {
+    ADD_SLOT( GhostCommunicationScheme , ghost_comm_scheme , INPUT);
+    ADD_SLOT( GhostUpdateCellLists , ghost_update_cell_lists , INPUT_OUTPUT);
+
+    inline void execute () override final
+    {      
+    }
+
+  };
+
+  // === register factory ===
+  ONIKA_AUTORUN_INIT(update_ghost_cell_list)
+  {
+    OperatorNodeFactory::instance()->register_factory(
+      "update_ghost_cell_list",
+      make_compatible_operator< UpdateGhostCellListsNode > );
+  }
+
+}
+
