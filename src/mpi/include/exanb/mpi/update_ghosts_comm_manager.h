2489c3239671140fa69a9bd53aea9826950acbe0
Thierry CARRARD <Thierry.CARRARD@cea.fr>
2026-04-07 09:32:14 +0200

    reverted to simpler and working bugfix

--- src/mpi/include/exanb/mpi/update_ghosts_comm_manager.h
+++ src/mpi/include/exanb/mpi/update_ghosts_comm_manager.h
@@ -394,17 +394,14 @@ namespace exanb
         return active_recvs;
       }
 
-      inline void resize_received_cells(auto * cells, auto * cell_allocator_ptr, auto create_particles )
+      inline void resize_received_cells(auto * const cells, const auto & cell_allocator, auto create_particles )
       {
         if constexpr ( create_particles )
         {
-          if( cells != nullptr && cell_allocator_ptr != nullptr )
+          const int nprocs = num_procs();
+          for(int p=0;p<nprocs;p++)
           {
-            const int nprocs = num_procs();
-            for(int p=0;p<nprocs;p++)
-            {
-              unpack_functors[p].resize_received_cells(cells,cell_allocator_ptr);
-            }
+            unpack_functors[p].resize_received_cells(cells,cell_allocator);
           }
         }
       }
